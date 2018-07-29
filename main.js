8b322aa9026a9ceb57867764fad9a65ff5564f2f
Jasmine Singh <jasmine.singh@jas.si>
2018-07-29 11:53:22 -0700

    added placeholders for jquery changes to template tags

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(function() {
 
+$('#art').prepend('<h3>Visual Development</h3>');
+// $('#art #carousel').  //put visdev images in a carousel
+// $('#art #contact').   //put thumbnail gallery of other illustrations (click on each to enlarge)
+
+//$('#contact form').submit //Make form submission functional
+
 });
 
 function eventListener () {
