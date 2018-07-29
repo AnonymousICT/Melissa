cb8aedcf08f157438008fbbc24bb7938e76c9e46
Jasmine Singh <jasmine.singh@jas.si>
2018-07-29 12:20:31 -0700

    changed last input on email form to a textarea

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 $(function() {
 
-$('#art').prepend('<h3>Visual Development</h3>');
 // $('#art #carousel').  //put visdev images in a carousel
-// $('#art #contact').   //put thumbnail gallery of other illustrations (click on each to enlarge)
 
 //$('#contact form').submit //Make form submission functional
 
