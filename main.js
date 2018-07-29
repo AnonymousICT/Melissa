bc263d67e5a77501d46f38e94fa9dcddf30e767e
Jasmine Singh <jasmine.singh@jas.si>
2018-07-29 16:04:16 -0700

    added photos to gallery via their thumbnails

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 $(function() {
 
-// $('#art #carousel').  //put visdev images in a carousel
 
-//$('#contact form').submit //Make form submission functional
+
+//put visdev images in a carousel
+// $('#art #carousel').
+
+//put illustration images in thumbnail gallery (hardcoding numbers for now)
+let galleryStr = "";
+for (let j=0; j<10; j++) {
+  galleryStr+='<a href="assets/ils/0'+j+'.png"><img src="assets/ils_thumbs/0'+j+'.png" class="thumb" /></a>';
+}
+for (let j=10; j<23; j++) {
+  galleryStr+='<a href="assets/ils/'+j+'.png"><img src="assets/ils_thumbs/'+j+'.png" class="thumb" /></a>';
+}
+
+$('#art #gallery').html(galleryStr);
+
+//Make form submission functional
+// $('#contact form').submit();
 
 });
 
