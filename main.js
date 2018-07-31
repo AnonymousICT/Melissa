e8e4a88034239b607350b0a45ec96c1098c35dfb
AnonymousICT <kuohoward@gmail.com>
2018-07-30 17:07:00 -0700

    resolved merge conflict

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-function renderTemplateContact() {
-temp = document.getElementById("contact")
+
+//displays visual development images
+// $('#art #visdev').
+
+//put illustration images in thumbnail gallery (hardcoding numbers for now)
+let galleryStr = "";
+for (let j=0; j<10; j++) {
+  galleryStr+='<a href="assets/ils/0'+j+'.png"><img src="assets/ils_thumbs/0'+j+'.png" class="thumb" /></a>';
 }
+for (let j=10; j<=23; j++) {
+  galleryStr+='<a href="assets/ils/'+j+'.png"><img src="assets/ils_thumbs/'+j+'.png" class="thumb" /></a>';
+}
+
+$('#art #gallery').append(galleryStr);
+
+//Make form submission functional
+// $('#contact form').submit();
+
+
 
 function eventListener () {
 
