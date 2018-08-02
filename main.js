7da2c0148ca58de986d7783a6b0460189520139d
jupigare <jasmine.singh@jas.si>
2018-08-02 12:35:25 -0700

    streamlined visdev pane by renaming assets and refactoring code

--- main.js
+++ main.js
@@ -5,205 +5,65 @@ function renderStartPage() {
     </section>`);
 }
 
+function addCarouselImages(folder,maxnum) {
+  let tempStr=`
+    <div id="${folder}-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
+    <ol class="carousel-indicators">
+      <li data-target="#${folder}-carousel" data-slide-to="0" class="active"></li>`;
+
+  for (let i=1; i<maxnum; i++) {
+    tempStr+= `<li data-target="#${folder}-carousel" data-slide-to="${i}"></li>`;
+  }
+
+  tempStr += `
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/visdev/${folder}/01.jpg" class="d-block w-100" />
+    </div>
+  `;
+  for (let j=2; j<maxnum+1; j++) {
+    tempStr+= `
+    <div class="carousel-item">
+      <img src="assets/visdev/${folder}/${('0' + j).slice(-2)}.jpg" class="d-block w-100" />
+    </div>`;
+  }
+  tempStr+=`
+              </div>
+            <a class="carousel-control-prev" href="#${folder}-carousel" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#${folder}-carousel" role="button" data-slide="next">
+              <span class="carousel-control-next-icon"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>`;
+
+  return tempStr;
+}
+
 function renderVisDevPage () {
-  $('main').html(`
+  let visDevStr = `
     <section id="visdev" role="region">
         <h2>Visual Development</h2>
         <div id="busstop">
-          <h3>Storyboards for "Bus Stop" Project</h3>
-          <div id="busstop-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-              <li data-target="#busstop-carousel" data-slide-to="0" class="active"></li>
-              <li data-target="#busstop-carousel" data-slide-to="1"></li>
-              <li data-target="#busstop-carousel" data-slide-to="2"></li>
-              <li data-target="#busstop-carousel" data-slide-to="3"></li>
-              <li data-target="#busstop-carousel" data-slide-to="4"></li>
-              <li data-target="#busstop-carousel" data-slide-to="5"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="assets/visdev/busstop/storybrd_BUS_STOP_1A.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-               <img src="assets/visdev/busstop/storybrd_BUS_STOP_1E.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                 <img src="assets/visdev/busstop/storybrd_BUS_STOP_1G.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/busstop/storybrd_BUS_STOP_1H.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                 <img src="assets/visdev/busstop/storybrd_BUS_STOP_1J.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/busstop/storybrd_BUS_STOP_1K.jpg" class="d-block w-100" />
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#busstop-carousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#busstop-carousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-
-        <div id="sync">
-          <h3>Storyboards for "SYNC" Project: Memory Leak 01</h3>
-          <div id="sync01-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-              <li data-target="#sync01-carousel" data-slide-to="0" class="active"></li>
-              <li data-target="#sync01-carousel" data-slide-to="1"></li>
-              <li data-target="#sync01-carousel" data-slide-to="2"></li>
-              <li data-target="#sync01-carousel" data-slide-to="3"></li>
-              <li data-target="#sync01-carousel" data-slide-to="4"></li>
-              <li data-target="#sync01-carousel" data-slide-to="5"></li>
-              <li data-target="#sync01-carousel" data-slide-to="6"></li>
-              <li data-target="#sync01-carousel" data-slide-to="7"></li>
-              <li data-target="#sync01-carousel" data-slide-to="8"></li>
-              <li data-target="#sync01-carousel" data-slide-to="9"></li>
-              <li data-target="#sync01-carousel" data-slide-to="10"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="assets/visdev/sync/sync01/1A.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1A02.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1A03.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1A04.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1B.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1B02.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1B03.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1C.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1D.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1D02.jpg" class="d-block w-100" />
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync01/1D03.jpg" class="d-block w-100" />
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#sync01-carousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#sync01-carousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
+          <h3>Storyboards for "Bus Stop" Project</h3>`;
+  visDevStr += addCarouselImages('busstop',7);
+  visDevStr += `
+        <div id="sync01">
+          <h3>Storyboards for "SYNC" Project: Memory Leak 01</h3>`;
+  visDevStr += addCarouselImages('sync01',11);
+  visDevStr += `
         <div id="sync07">
-          <h3>Storyboards for "SYNC" Project: Memory Leak 07</h3>
-          <div id="sync07-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-              <li data-target="#sync07-carousel" data-slide-to="0" class="active"></li>
-              <li data-target="#sync07-carousel" data-slide-to="1"></li>
-              <li data-target="#sync07-carousel" data-slide-to="2"></li>
-              <li data-target="#sync07-carousel" data-slide-to="3"></li>
-              <li data-target="#sync07-carousel" data-slide-to="4"></li>
-              <li data-target="#sync07-carousel" data-slide-to="5"></li>
-              <li data-target="#sync07-carousel" data-slide-to="6"></li>
-              <li data-target="#sync07-carousel" data-slide-to="7"></li>
-              <li data-target="#sync07-carousel" data-slide-to="8"></li>
-              <li data-target="#sync07-carousel" data-slide-to="9"></li>
-              <li data-target="#sync07-carousel" data-slide-to="10"></li>
-              <li data-target="#sync07-carousel" data-slide-to="11"></li>
-              <li data-target="#sync07-carousel" data-slide-to="12"></li>
-              <li data-target="#sync07-carousel" data-slide-to="13"></li>
-              <li data-target="#sync07-carousel" data-slide-to="14"></li>
-              <li data-target="#sync07-carousel" data-slide-to="15"></li>
-              <li data-target="#sync07-carousel" data-slide-to="16"></li>
-              <li data-target="#sync07-carousel" data-slide-to="17"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="assets/visdev/sync/sync07/7.5A.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7.5A02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7.5A03.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7.5A04.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7.5B.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7.5B02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7A.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7A02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7B.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7C.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7D.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7E.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7E02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7G.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7G02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7H.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7H02.jpg" class="d-block w-100" /></a>
-              </div>
-              <div class="carousel-item">
-                <img src="assets/visdev/sync/sync07/7H03.jpg" class="d-block w-100" /></a>
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#sync07-carousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#sync07-carousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-
+          <h3>Storyboards for "SYNC" Project: Memory Leak 07</h3>`;
+  visDevStr += addCarouselImages('sync07',18);
+  visDevStr += `
         </div>
-      </section>
-  `);
+      </section>`;
+  $('main').html(visDevStr);
+
 }
 
 function renderIllustrationPage () {
